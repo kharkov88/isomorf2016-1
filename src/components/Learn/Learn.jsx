d9014a7f0f4513e10999b289f1e711d2437cc5f0
Artem Vanakov <artem.vanakov@i3design.com>
2018-05-17 16:34:40 +0300

    try animate component Learn

--- src/components/Learn/Learn.jsx
+++ src/components/Learn/Learn.jsx
@@ -4,6 +4,25 @@ import { Link } from 'react-router'
 import './Learn.css'
 
 class Learn extends React.Component {
+  componentDidMount () {
+    let htmlCollection = document.querySelectorAll('.learn-item')
+    htmlCollection.forEach( item => {
+      item.addEventListener('transitionend', function () {
+        console.log(this)
+        this.parentNode.removeChild(this)
+      })      
+    })
+
+    htmlCollection.forEach( item => {
+      item.style.opacity = 0
+    })
+  }
+  componentWillUnmount () {
+    let htmlCollection = document.querySelectorAll('.learn-item')
+    htmlCollection.forEach( item => {
+      //item.style.right = 0
+    })
+  }
   constructor (props) {
     super(props)
     this.state = {
@@ -21,9 +40,9 @@ class Learn extends React.Component {
         return (
           <div className='learn'>
             <div className='learn-page'><h2>content learn</h2></div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div className='learn-item'>link1</div>
+            <div className='learn-item'>link2</div>
+            <div className='learn-item'>link3</div>
             
           </div>
         )
