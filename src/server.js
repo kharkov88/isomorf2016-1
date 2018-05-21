bd4a230507ca2d5cd95c8527bc67637525d4d94a
Artem Vanakov <artem.vanakov@i3design.com>
2018-05-21 15:10:53 +0300

    fix

--- src/server.js
+++ src/server.js
@@ -33,7 +33,7 @@ function renderHTML (componentHTML) {
       <html>
       <head>
           <meta charset="utf-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">  
           <title>Hello React</title>
           <link rel="stylesheet" href="${assetUrl}/public/assets/styles.css">
       </head>
