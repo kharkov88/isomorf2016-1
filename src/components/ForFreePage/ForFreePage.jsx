73bdf1a6f821baaa1b2eedc2d49fe4126977c0e7
Artem Vanakov <artem.vanakov@i3design.com>
2018-06-14 17:48:05 +0300

    add submit click for orfderPage

--- src/components/ForFreePage/ForFreePage.jsx
+++ src/components/ForFreePage/ForFreePage.jsx
@@ -1,4 +1,4 @@
-import React, { PropTypes, Component } from 'react'
+import React, { Component } from 'react'
 import { Link, Route, Switch } from 'react-router-dom'
 import Header from 'components/Header'
 import Footer from 'components/Footer'
@@ -16,38 +16,38 @@ class ForFreePage extends Component {
     return (
       <div className='for-free-page bg-1'>
         <Header />
-        <main className="container main">
-            <div className="description animated zoomIn">
-               <h1 className="animated zoomIn">Looking for free options</h1>
+        <main className='container main'>
+          <div className='description animated zoomIn'>
+            <h1 className='animated zoomIn'>Looking for free options</h1>
+          </div>
+          <div className='actions row'>
+            <div className='grid'>
+              <figure className='effect-chico animated bounceInLeft'>
+                <a href='https://html2amp.mobilizetoday.com/' target='_blank'>
+                  <img src='/img/block-image/3/converter.svg' alt='Free HTML to AMP converter' />
+                  <figcaption>
+                    <h2>Free HTML to AMP converter</h2>
+                  </figcaption>
+                </a>
+              </figure>
+              <figure className='effect-chico animated bounceInUp'>
+                <Link to='/learn/compare'>
+                  <img src='/img/block-image/3/compare.svg' alt='Compare load speed of any webpage with AMP' />
+                  <figcaption>
+                    <h2>Compare load speed of your webpage with AMP</h2>
+                  </figcaption>
+                </Link>
+              </figure>
+              <figure className='effect-chico animated bounceInRight'>
+                <Link to='/order'>
+                  <img src='/img/block-image/3/order.svg' alt='Request a quote (for free!)' />
+                  <figcaption>
+                    <h2>Request a quote for free!</h2>
+                  </figcaption>
+                </Link>
+              </figure>
             </div>
-            <div className="actions row">
-                <div className="grid">
-                    <figure className="effect-chico animated bounceInLeft">
-                        <a href="https://html2amp.mobilizetoday.com/" target="_blank">
-                            <img src="/img/block-image/3/converter.svg" alt="Free HTML to AMP converter" />
-                            <figcaption>
-                                <h2>Free HTML to AMP converter</h2>
-                            </figcaption>
-                            </a>
-                    </figure>
-                    <figure className="effect-chico animated bounceInUp">
-                        <Link to="/learn/compare">
-                            <img src="/img/block-image/3/compare.svg" alt="Compare load speed of any webpage with AMP" />
-                            <figcaption>
-                                <h2>Compare load speed of your webpage with AMP</h2>
-                            </figcaption>
-                            </Link>
-                    </figure>
-                    <figure className="effect-chico animated bounceInRight">
-                        <Link to="/order">
-                            <img src="/img/block-image/3/order.svg" alt="Request a quote (for free!)" />
-                            <figcaption>
-                                <h2>Request a quote for free!</h2>
-                            </figcaption>
-                            </Link>
-                    </figure>
-                </div>
-            </div>
+          </div>
         </main>
         <Footer url={url} />
       </div>
@@ -56,4 +56,3 @@ class ForFreePage extends Component {
 }
 
 export default ForFreePage
-
--- src/components/OrderPage/OrderPage.jsx
+++ src/components/OrderPage/OrderPage.jsx
@@ -9,6 +9,11 @@ import linesAnimation from './bg-2-js/3d-lines-animation'
 import projector from './bg-2-js/projector'
 import canvasRenderer from './bg-2-js/canvas-renderer'
 
+let config = {
+  style: {
+    margin: '0 auto'
+  }
+}
 class OrderPage extends Component {
   constructor (props) {
     super(props)
@@ -33,25 +38,7 @@ class OrderPage extends Component {
       <div className='order-page bg-2'>
         <Header />
         <main className='container main'>
-          <h1 className='animated zoomIn'>Get a quote</h1>
-          <section className='order-form animated zoomIn'>
-            <div className='order-form-description'>Successful and profitable projects beginning here</div>
-            <form id='order-form' action='order.html' method='post'>
-              <div className='input-block'>
-                <div className='left-side'>
-                  <input type='text' id='order-name' name='order-name' className='form-control' placeholder='Name' />
-                  <input type='text' id='order-url' name='order-url' className='form-control' placeholder='URL' />
-                  <input type='email' id='order-email' name='order-email' className='form-control' placeholder='Email' />
-                </div>
-                <div className='right-side'>
-                  <textarea id='order-message' name='order-message' className='form-control' placeholder='Tell more about if you wish' />
-                </div>
-              </div>
-              <div className='button-block'>
-                <button type='submit' id='order-submit' className='order-form-submit'><span>SUBMIT REQUEST</span> <i className='material-icons'>chevron_right</i></button>
-              </div>
-            </form>
-          </section>
+            <CustomForm />
         </main>
         <div id='canvas' className='gradient' />
         <Footer url={url} />
@@ -60,4 +47,99 @@ class OrderPage extends Component {
   }
 }
 
+class CustomForm extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      valueName: '',
+      valueUrl: '',
+      valueEmail: '',
+      valueMsg: '',
+      startFetch: false,
+      response: ''
+    }
+
+    this.handleChangeName = this.handleChangeName.bind(this)
+    this.handleChangeUrl = this.handleChangeUrl.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleChangeName(event) {
+    this.setState({valueName: event.target.value});
+  }
+  handleChangeUrl(event) {
+    this.setState({valueUrl: event.target.value});
+  }
+  handleChangeEmail(event) {
+    this.setState({valueEmail: event.target.value});
+  }
+  handleChangeMsg(event) {
+    this.setState({valueMsg: event.target.value});
+  }
+
+  handleSubmit(event) {
+    //alert('A name was submitted: ' + this.state.valueName);
+    this.setState({startFetch: !this.state.startFetch})
+    setTimeout(() => {
+      this.setState({
+        response: 'Successful!',
+        startFetch: !this.state.startFetch
+      })
+    },1500)
+    setTimeout(() => {
+      document.querySelector('.logo a').click()
+    },2000)
+    event.preventDefault();
+  }
+
+  render() {
+    return(
+      this.state.response.length>1
+      ? <h1 style={config.style}>{this.state.response}</h1>
+      :
+        this.state.startFetch
+        ? <h1 style={config.style}>loading...</h1>
+        :
+        <div style={config.style}>
+          <h1 className='animated zoomIn'>Get a quote</h1>
+          <section className='order-form animated zoomIn'>
+            <div className='order-form-description'>Successful and profitable projects beginning here</div>
+            <form id='order-form' onSubmit={this.handleSubmit}>
+              <div className='input-block'>
+                <div className='left-side'>
+                  <input 
+                    type='text'
+                    id='order-name' 
+                    name='order-name' 
+                    className='form-control' 
+                    placeholder='Name'
+                    value={this.state.valueName}
+                    onChange={this.handleChangeName}
+                  />
+                  <input 
+                    type='text' 
+                    id='order-url' 
+                    name='order-url' 
+                    className='form-control' 
+                    placeholder='URL'
+                    value={this.state.valueUrl}
+                    onChange={this.handleChangeUrl}
+                  />
+                  <input type='email' id='order-email' name='order-email' className='form-control' placeholder='Email' />
+                </div>
+                <div className='right-side'>
+                  <textarea id='order-message' name='order-message' className='form-control' placeholder='Tell more about if you wish' />
+                </div>
+              </div>
+              <div className='button-block'>
+                <button type='submit' id='order-submit' className='order-form-submit'>
+                  <span>SUBMIT REQUEST</span>
+                  <i className='material-icons'>chevron_right</i>
+                </button>
+              </div>
+            </form>
+          </section>
+        </div>
+      )
+  }
+}
 export default OrderPage
